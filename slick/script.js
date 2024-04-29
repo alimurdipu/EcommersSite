46cbd847bec3219d885482e148f00bdb28a6eaab
Alimur Razi <138320899+alimurdipu@users.noreply.github.com>
2024-04-29 23:06:12 +0600

    set end

---
+++ slick/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.item-container').slick({
+    //   setting-name: setting-value
+    arrows: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: ".previous",
+    nextArrow: ".next"
+    });
+  });
+      
